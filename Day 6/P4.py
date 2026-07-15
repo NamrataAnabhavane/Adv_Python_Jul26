a3e452c83b30ed4c852694ea5a783f7858cfd215
Namrata Anabhavane <g24.namrata.anabhavane@gnkhalsa.edu.in>
2026-07-15 18:36:15 +0530

    OJT

---
+++ Day 6/P4.py
@@ -0,0 +1,23 @@
+class CountDown:
+    def __init__(self,start):
+        self.current = start
+        self.start = start
+    
+    def __iter__(self):
+        """Return the iterator object itself."""
+        return self
+    
+    def __next__(self):
+        if self.current <1:
+            raise StopIteration
+        
+        value = self.current
+        self.current -= 1
+        return value
+print("Countdown")
+countdown = CountDown(5)
+print(next(countdown))
+print(next(countdown))
+print(next(countdown))
+print(next(countdown))
+print(next(countdown))
