5efec38b4d88738657356121b9c3a5cf107aab81
Namrata Anabhavane <g24.namrata.anabhavane@gnkhalsa.edu.in>
2026-07-24 18:24:04 +0530

    Sync vs Async task

---
+++ Day 13/P1.py
@@ -0,0 +1,37 @@
+import time
+import asyncio
+def sync_work():
+    print("Starting Sychronous work")
+
+    print("Taskk 1: Starting")
+    time.sleep(2)
+    print("Task 1 done")
+
+    print("Taskk 2: Starting")
+    time.sleep(2)
+    print("Task 2 done")
+
+    print("All Synchronous work done")
+
+#sync_work()
+
+async def async_work():
+    print("Starting Asychronous work")
+
+    print("Task 1: Starting")
+    await asyncio.sleep(2)
+    print("Task 1 done")
+
+    print("Taskk 2: Starting")
+    await asyncio.sleep(2)
+    print("Task 2 done")
+
+    print("All Asynchronous work done")
+
+start = time.time()
+sync_work()
+print(f"Synchronous: {time.time() - start:.2f}s")
+
+start1 = time.time()
+asyncio.run(async_work())
+print(f"Asynchronous: {time.time() - start1:.2f}s")
