0a256c0e5511d00868e569059c9d5f227802c693
Namrata Anabhavane <g24.namrata.anabhavane@gnkhalsa.edu.in>
2026-08-06 19:31:46 +0530

    Path Parameter

--- Day 19/P2.py
+++ Day 19/P2.py
@@ -13,7 +13,15 @@ async def root():
 
 @app.get("/hello/{name}")
 async def say_hello(name:str):
-    return {"message": "Hello, {name}!"}
+    return {"message": f"Hello, {name}!"}
+
+@app.get("/users/{user_id}/orders/{order_id}")
+async def get_order(user_id:int,order_id:int):
+    return {
+        "user_id":user_id,
+        "order_id":order_id,
+        "order":f"Order {order_id} for user {user_id}"
+    }
 
 if __name__ == "__main__":
     uvicorn.run(
