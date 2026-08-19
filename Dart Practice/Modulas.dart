6d0c9c4a7e7f670561d16ccdff4f1f43cc1da414
Namrata Anabhavane <g24.namrata.anabhavane@gnkhalsa.edu.in>
2026-08-19 19:18:13 +0530

    Modulas

---
+++ Dart Practice/Modulas.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Example 1
+  int result = 10 % 3;
+  print(result);
+
+  // Example 2
+  int number = 10;
+  if (number % 2 == 0) {
+    print("Even");
+  } else {
+    print("Odd");
+  }
+}
