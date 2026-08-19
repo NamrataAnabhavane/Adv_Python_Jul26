81ef084dc833d11035d2821bba2b55a02f3ac450
Namrata Anabhavane <g24.namrata.anabhavane@gnkhalsa.edu.in>
2026-08-19 19:17:54 +0530

    Marks

---
+++ Dart Practice/Marks.dart
@@ -0,0 +1,18 @@
+void main() {
+  String StudentName = "Namrata";
+  int english = 90;
+  int maths = 95;
+  int science = 80;
+
+  int total = english + maths + science;
+
+  var percentage = total / 3;
+
+  print("=====Marks Report=====");
+  print("Student Name : $StudentName");
+  print("English : $english");
+  print("Maths : $maths");
+  print("Science : $science");
+  print("Total : $total");
+  print("Percentage : $percentage");
+}
