22b3206aa62b8cbf0db8ed33c375bd5224c71ab7
Namrata Anabhavane <g24.namrata.anabhavane@gnkhalsa.edu.in>
2026-08-19 19:17:38 +0530

    Integer

---
+++ Dart Practice/Integer.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Example 1
+  int result = 10 ~/ 3;
+  print(result);
+
+  // Example 2
+  int students = 20;
+  int studentsPerGroup = 4;
+  int groups = students ~/ studentsPerGroup;
+  print("Number of Groups: $groups");
+}
