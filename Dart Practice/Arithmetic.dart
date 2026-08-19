ad825dc009c83a893622404ced4e609178806006
Namrata Anabhavane <g24.namrata.anabhavane@gnkhalsa.edu.in>
2026-08-19 19:16:24 +0530

    Arithmetic

---
+++ Dart Practice/Arithmetic.dart
@@ -0,0 +1,29 @@
+void main() {
+  int price = 500;
+  int deliveryCharge = 50;
+  int total = price + deliveryCharge;
+  print("Addition");
+  print("Total: $total");
+
+  int totalMarks = 100;
+  int obtainedMarks = 75;
+  int remaining = totalMarks - obtainedMarks;
+  print("Subtraction");
+  print(remaining);
+
+  int Price = 200;
+  int quantity = 3;
+  int Total = Price * quantity;
+  print("Multiplication");
+  print("Total: ₹$Total");
+
+  int TOtal = 10;
+  int students = 2;
+  var result = TOtal / students;
+  print("Division");
+  print(result);
+
+  int Result = 10 ~/ 3;
+  print("Result");
+  print(Result);
+}
