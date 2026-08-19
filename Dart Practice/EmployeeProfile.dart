95fc2c822d3f6e244a2353116bc53b1f7f77080e
Namrata Anabhavane <g24.namrata.anabhavane@gnkhalsa.edu.in>
2026-08-19 19:17:21 +0530

    Employee Profile

---
+++ Dart Practice/EmployeeProfile.dart
@@ -0,0 +1,25 @@
+/* Exercise 1 — Employee Profile
+Create variables for:
+Employee ID
+Name
+Department
+Salary
+Experience
+Working status
+Display them using string interpolation*/
+
+void main() {
+  int EmployeeId = 101;
+  String Name = "Namrata";
+  String Department = "Data Science";
+  int Salary = 9000000;
+  int Experience = 3;
+  String WorkingStatus = "Full Time";
+
+  print("Employee Id : $EmployeeId");
+  print("Name : $Name");
+  print("Department : $Department");
+  print("Salary : $Salary");
+  print("Experience : $Experience");
+  print("WorkingStatus : $WorkingStatus");
+}
