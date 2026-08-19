232f9bdd7a7b27c29ac442a6e81742f478224cfc
Namrata Anabhavane <g24.namrata.anabhavane@gnkhalsa.edu.in>
2026-08-19 18:25:39 +0530

    Printing Values

---
+++ Dart Practice/StudentProgram.dart
@@ -0,0 +1,20 @@
+void main() {
+  String name = "Namrata";
+  int age = 20;
+  String course = "BSc CS";
+  double percentage = 85.5;
+  bool isPassed = true;
+
+  final studentId = 101;
+  const maxMarks = 100;
+
+  print("=====STUDENT PROFILE=====");
+
+  print("Student ID : $studentId");
+  print("Name : $name");
+  print("Age : $age");
+  print("Course : $course");
+  print("Percentage : $percentage");
+  print("Paseed : $isPassed");
+  print("Max Marks : $maxMarks");
+}
